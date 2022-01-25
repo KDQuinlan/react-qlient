8556f5ee423b5074a294a548db09d3681e565d28
Kane Longbottom <longbottomk@outlook.com>
2022-01-25 21:48:15 +0000

    revamp project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
+import React from "react";
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -74,9 +74,9 @@ export default function Home() {
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M4 6h16M4 12h16M4 18h16"
                     />
                   </svg>
